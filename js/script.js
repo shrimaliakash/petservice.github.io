aa60eabd5eb29a5cb0bb9212db3a17a909a97b97
Akash Govindbhai Shrimali <akash@nuwaveindia.net>
2022-11-22 23:19:54 +0530

    click shopping cart button cart list display done

--- js/script.js
+++ js/script.js
@@ -2,17 +2,27 @@ let loginForm = document.querySelector('.header .login-form');
 document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
     navbar.classList.remove('active');
+    shoppingCart.classList.remove('active');
 }
 
 let navbar = document.querySelector('.header .navbar');
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     loginForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.header .shopping-cart');
+document.querySelector('#shopping-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 window.onscroll = () => {
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
+    shoppingCart.classList.remove('active');
 
     if(window.scrollY > 0) {
         document.querySelector('.header').classList.add('active');
