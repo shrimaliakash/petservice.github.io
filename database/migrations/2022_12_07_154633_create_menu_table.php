99ae9cf48a84113af8bef301b3d2e5b23ca2c90e
Akash Govindbhai Shrimali <akash@nuwaveindia.net>
2022-12-07 23:25:29 +0530

    menu migratuion complete with menu controller and model create

---
+++ database/migrations/2022_12_07_154633_create_menu_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url ', 100)->nullable();
+            $table->text('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
